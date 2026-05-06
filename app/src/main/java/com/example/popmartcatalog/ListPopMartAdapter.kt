60604b84ddc6a5a1feb2a94619d0cadfb7e4d667
meizaluna18-debug <meizaluna1805@gmail.com>
2026-05-06 22:19:29 +0800

    Menambahkan fungsi klik item di Adapter

--- app/src/main/java/com/example/popmartcatalog/ListPopMartAdapter.kt
+++ app/src/main/java/com/example/popmartcatalog/ListPopMartAdapter.kt
@@ -1,21 +1,23 @@
 package com.example.popmartcatalog
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.util.Locale
 
 class ListPopMartAdapter(private val listPopMart: ArrayList<PopMart>) : RecyclerView.Adapter<ListPopMartAdapter.ListViewHolder>() {
 
     class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        // ID sekarang SUDAH SINKRON dengan item_popmart.xml kamu!
         val imgPhoto: ImageView = itemView.findViewById(R.id.img_popmart)
         val tvName: TextView = itemView.findViewById(R.id.txt_nama_karakter)
         val tvSeries: TextView = itemView.findViewById(R.id.txt_series)
         val tvPrice: TextView = itemView.findViewById(R.id.txt_harga)
-        val tvStatus: TextView = itemView.findViewById(R.id.txt_status) // Aku tambahkan sekalian untuk status
+        val tvStatus: TextView = itemView.findViewById(R.id.txt_status)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
@@ -23,17 +25,23 @@ class ListPopMartAdapter(private val listPopMart: ArrayList<PopMart>) : Recycler
         return ListViewHolder(view)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val (name, series, price, status, image) = listPopMart[position]
 
-        // Memasukkan data dari gudang ke dalam desain XML
         holder.imgPhoto.setImageResource(image)
         holder.tvName.text = name
         holder.tvSeries.text = series
         holder.tvStatus.text = status
 
-        // Menambahkan teks "Rp" agar harga tampil cantik
-        holder.tvPrice.text = "Rp $price"
+        val formatRupiah = java.text.NumberFormat.getNumberInstance(Locale.forLanguageTag("id-ID"))
+        holder.tvPrice.text = "Rp ${formatRupiah.format(price)}"
+
+        holder.itemView.setOnClickListener {
+            val intentDetail = Intent(holder.itemView.context, DetailActivity::class.java)
+            intentDetail.putExtra(DetailActivity.EXTRA_POPMART, listPopMart[holder.bindingAdapterPosition])
+            holder.itemView.context.startActivity(intentDetail)
+        }
     }
 
     override fun getItemCount(): Int = listPopMart.size
