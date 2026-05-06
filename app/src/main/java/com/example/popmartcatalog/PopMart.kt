d70071a5cdbd8020b57ea49febe70b6b03fb251a
meizaluna18-debug <meizaluna1805@gmail.com>
2026-05-06 15:26:55 +0800

    Setup Parcelize dan Model Data untuk Intent DetailActivity serta mengubah kesalahan gambar Labubu

--- app/src/main/java/com/example/popmartcatalog/PopMart.kt
+++ app/src/main/java/com/example/popmartcatalog/PopMart.kt
@@ -1,9 +1,13 @@
 package com.example.popmartcatalog
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class PopMart(
     val name: String,
     val series: String,
     val price: Int,
     val status: String,
     val image: Int
-)
+) : Parcelable
